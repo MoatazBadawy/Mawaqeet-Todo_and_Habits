02a3dd0d716f76a2b10fc78f45c27612505c3c17
MoatazBadawy <moataz.egy101@gmail.com>
2023-03-02 10:55:33 +0300

    add datastore and di provider for it

---
+++ app/src/main/java/com/moataz/mawaqeet/di/utils/DatabaseConstant.kt
@@ -0,0 +1,6 @@
+package com.moataz.mawaqeet.di.utils
+
+object DatabaseConstant {
+    const val DATABASE_NAME = "mohareb.db"
+    const val SHARED_PREFERENCES_NAME = "mohareb.ds"
+}
--- app/src/main/java/com/moataz/mawaqeet/main/MainActivity.kt
+++ app/src/main/java/com/moataz/mawaqeet/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.moataz.mawaqeet
+package com.moataz.mawaqeet.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.moataz.mawaqeet.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
