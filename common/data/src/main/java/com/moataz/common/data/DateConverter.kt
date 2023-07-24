513576df52194b911e0164ec29d244de0b4f8eaa
MoatazBadawy <moataz.egy101@gmail.com>
2023-07-25 02:19:34 +0300

    feat: create app database

---
+++ common/data/src/main/java/com/moataz/common/data/DateConverter.kt
@@ -0,0 +1,16 @@
+package com.moataz.common.data
+
+import androidx.room.TypeConverter
+import java.util.Date
+
+class DateConverter {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return if (value == null) null else Date(value)
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time
+    }
+}
---
+++ common/data/src/main/java/com/moataz/common/data/MawaqeetDatabase.kt
@@ -0,0 +1,28 @@
+package com.moataz.common.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [
+        HabitEntity::class,
+        TodoEntity::class,
+    ],
+    version = 2,
+)
+@TypeConverters(DateConverter::class)
+abstract class MawaqeetDatabase : RoomDatabase() {
+    abstract fun habitsDao(): HabitsDao
+    abstract fun todosDao(): TodosDao
+}
+
+object MawaqeetDatabaseFactory {
+    fun roomDatabase(context: Context) = Room.databaseBuilder(
+        context,
+        MawaqeetDatabase::class.java,
+        "mawaqeet.db",
+    ).build()
+}
