362bbc60059af6f9dc68af95992cf69d465fdfd0
MoatazBadawy <moataz.egy101@gmail.com>
2023-07-25 15:22:49 +0300

    feat: add habits entity for room database

--- common/data/src/main/java/com/moataz/common/data/MawaqeetDatabase.kt
+++ common/data/src/main/java/com/moataz/common/data/MawaqeetDatabase.kt
@@ -5,6 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.moataz.habits.data.local.HabitEntity
 
 @Database(
     entities = [
