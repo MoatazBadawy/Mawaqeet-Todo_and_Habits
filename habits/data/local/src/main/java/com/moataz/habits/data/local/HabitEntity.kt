9a1d6e078eb06f36afd61cd12101d045a91397de
MoatazBadawy <moataz.egy101@gmail.com>
2023-08-01 16:37:11 +0300

    update: remove unnecessary Keep annotation in entities classes.

--- habits/data/local/src/main/java/com/moataz/habits/data/local/HabitEntity.kt
+++ habits/data/local/src/main/java/com/moataz/habits/data/local/HabitEntity.kt
@@ -1,13 +1,11 @@
 package com.moataz.habits.data.local
 
-import androidx.annotation.Keep
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.moataz.habits.data.local.utils.EntityConstant.HABIT_TABLE
 import com.moataz.habits.data.local.utils.EntityConstant.NEXT_RESET_DATE_HABIT
 import java.util.Date
 
-@Keep
 @Entity(tableName = HABIT_TABLE)
 data class HabitEntity(
     @PrimaryKey(autoGenerate = true)
--- todos/data/local/src/main/java/com/moataz/todos/data/local/TodoEntity.kt
+++ todos/data/local/src/main/java/com/moataz/todos/data/local/TodoEntity.kt
@@ -1,11 +1,9 @@
 package com.moataz.todos.data.local
 
-import androidx.annotation.Keep
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.moataz.todos.data.local.utils.EntityConstant.TODO_TABLE
 
-@Keep
 @Entity(tableName = TODO_TABLE)
 data class TodoEntity(
     @PrimaryKey(autoGenerate = true)
