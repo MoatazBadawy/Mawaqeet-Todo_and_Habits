6ffd638cb81f6e36074f4ec172a3e98e3c18f71b
MoatazBadawy <moataz.egy101@gmail.com>
2023-12-08 04:41:55 +0200

    feat-todos: add trailingIcon to add the ability to remove text once.

--- todos/ui/view/src/main/java/com/moataz/todos/ui/view/screens/todo_edit/TodoEditScreen.kt
+++ todos/ui/view/src/main/java/com/moataz/todos/ui/view/screens/todo_edit/TodoEditScreen.kt
@@ -8,20 +8,29 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
@@ -67,7 +76,26 @@ fun TodoEditScreen(
                         cursorColor = Black,
                         textColor = Black,
                         focusedLabelColor = Black,
-                    )
+                    ),
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        imeAction = ImeAction.Done,
+                        keyboardType = KeyboardType.Text
+                    ),
+                    trailingIcon = {
+                        if (viewModel.todoTitle.value.isNotEmpty()) {
+                            IconButton(
+                                onClick = { viewModel.todoTitle.value = "" },
+                                modifier = Modifier
+                                    .clip(MaterialTheme.shapes.small)
+                                    .size(18.dp)
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Clear,
+                                    contentDescription = null
+                                )
+                            }
+                        }
+                    }
                 )
 
                 Spacer(modifier = Modifier.height(16.dp))
