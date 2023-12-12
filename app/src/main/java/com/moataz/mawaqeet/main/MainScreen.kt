67c2ad8a91799687e063adc09da5de7ac8b5723a
MoatazBadawy <moataz.egy101@gmail.com>
2023-12-12 05:19:34 +0200

    feat: refactor bottomNavigation

--- app/src/main/java/com/moataz/mawaqeet/main/MainScreen.kt
+++ app/src/main/java/com/moataz/mawaqeet/main/MainScreen.kt
@@ -9,12 +9,12 @@ import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.moataz.mawaqeet.R
 import com.moataz.mawaqeet.main.navigation.MainNavHost
@@ -25,62 +25,56 @@ import com.moataz.todos.ui.view.theme.Black
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
-    val selectedItem = remember { mutableStateOf(MainScreens.HabitsNavGraph.route) }
+    val screens = listOf(MainScreens.HabitsNavGraph.route, MainScreens.TodosNavGraph.route)
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
 
     Scaffold(
         bottomBar = {
             BottomNavigation(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(60.dp),
+                    .height(62.dp),
                 backgroundColor = Color(0xFFF8F8F8),
                 contentColor = Color.Unspecified
             ) {
-                BottomNavigationItem(
-                    icon = {
-                        Icon(
-                            painterResource(id = R.drawable.ic_habits),
-                            contentDescription = null,
-                            modifier = Modifier.size(24.dp),
-                        )
-                    },
-                    selected = selectedItem.value == MainScreens.HabitsNavGraph.route,
-                    selectedContentColor = Black,
-                    unselectedContentColor = Color(0xFFABABAB),
-                    onClick = {
-                        if (selectedItem.value != MainScreens.HabitsNavGraph.route) {
-                            selectedItem.value = MainScreens.HabitsNavGraph.route
-                            navController.navigate(MainScreens.HabitsNavGraph.route) {
-                                popUpTo(navController.graph.startDestinationId)
-                                launchSingleTop = true
+                screens.forEach { screen ->
+                    BottomNavigationItem(
+                        icon = {
+                            Icon(
+                                painterResource(
+                                    id = if (screen == MainScreens.HabitsNavGraph.route)
+                                        R.drawable.ic_habits else R.drawable.ic_todos
+                                ),
+                                contentDescription = null,
+                                modifier = Modifier.size(24.dp),
+                            )
+                        },
+                        selected = currentRoute == screen,
+                        selectedContentColor = Black,
+                        unselectedContentColor = Color(0xFFABABAB),
+                        onClick = {
+                            if (currentRoute != screen) {
+                                navController.navigate(screen) {
+                                    popUpTo(navController.graph.startDestinationId)
+                                    launchSingleTop = true
+                                }
                             }
-                        }
-                    },
-                )
-                BottomNavigationItem(
-                    icon = {
-                        Icon(
-                            painterResource(id = R.drawable.ic_todos),
-                            contentDescription = null,
-                            modifier = Modifier.size(24.dp),
-                        )
-                    },
-                    selected = selectedItem.value == MainScreens.TodosNavGraph.route,
-                    selectedContentColor = Black,
-                    unselectedContentColor = Color(0xFFABABAB),
-                    onClick = {
-                        if (selectedItem.value != MainScreens.TodosNavGraph.route) {
-                            selectedItem.value = MainScreens.TodosNavGraph.route
-                            navController.navigate(MainScreens.TodosNavGraph.route) {
-                                popUpTo(navController.graph.startDestinationId)
-                                launchSingleTop = true
-                            }
-                        }
-                    },
-                )
+                        },
+                    )
+                }
             }
         }
     ) {
         MainNavHost(navController)
     }
 }
+
+/**
+ * /\     /\
+ *{  `---'  }
+ *{  O   O  }
+ *  >  V  <
+ * `-.....-'
+ *   "meow"
+ */
